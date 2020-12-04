8e43f69e8b09ddd812e7faef8fd166dd35bfe4a5
Nils Dunkel <InimicusD@googlemail.com>
2020-12-04 17:11:56 +0100

    Add project files.

---
+++ Advent of Code Cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using Advent_of_Code_Cs.Days;
+using System;
+
+namespace Advent_of_Code_Cs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("What Day should be executed?\n");
+			char day = Console.ReadKey().KeyChar;
+
+			switch (day)
+			{
+				case '1':
+					new Day1();
+					break;
+				default:
+					Console.WriteLine("\nThat's not a number you doofus.");
+					break;
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
