9a5db5e3d1446e94377f79ee45dafb21736e7978
Nils Dunkel <InimicusD@googlemail.com>
2020-12-12 23:11:17 +0100

    refactor(Day2): Optimized Day2 and made it more readable.

--- Advent of Code Cs/Days/Day2.cs
+++ Advent of Code Cs/Days/Day2.cs
@@ -8,54 +8,66 @@ using System.Threading.Tasks;
 
 namespace Advent_of_Code_Cs.Days
 {
+
 	public class Day2
 	{
-		public Day2()
+		public struct Password
 		{
-			IEnumerable<string> lines = ParseInput();
-
-			Day2A(lines);
-			Day2B(lines);
-		}
-
-		private IEnumerable<string> ParseInput()
-		{
-			IEnumerable<string> lines = File.ReadLines(@"S:\Users\Nils\source\repos\Advent of Code Cs\Advent of Code Cs\Inputs\Day2_Input.txt", Encoding.UTF8);
-			return lines;
-		}
-
-		private static void Day2A(IEnumerable<string> lines)
-		{
-			Regex pwRegex = new Regex(@"(\d+)-(\d+) (\w): (\w+)");
-			int valids = 0;
-
-			foreach (string line in lines)
+			public Password(string text, char checker, int min, int max)
 			{
-				Match match = pwRegex.Match(line);
-
-				int count = match.Groups[4].Value.Count(c => c == match.Groups[3].Value.ElementAt(0));
-				if (count >= int.Parse(match.Groups[1].Value) && count <= int.Parse(match.Groups[2].Value))
-					valids++;
+				Text = text;
+				Checker = checker;
+				Min = min;
+				Max = max;
 			}
 
+			public string Text;
+			public char Checker;
+			public int Min;
+			public int Max;
+		}
+
+		private static Regex regex = new Regex(@"(\d+)-(\d+) (\w): (\w+)");
+
+		public Day2()
+		{
+			IEnumerable<Password> passwords = ParseInput();
+
+			Day2A(passwords);
+			Day2B(passwords);
+		}
+
+		private IEnumerable<Password> ParseInput()
+		{
+			IEnumerable<string> lines = File.ReadLines(@"S:\Users\Nils\source\repos\Advent of Code Cs\Advent of Code Cs\Inputs\Day2_Input.txt", Encoding.UTF8);
+			return lines.Select(line =>
+			{
+				Match match = regex.Match(line);
+				return new Password(match.Groups[4].Value, match.Groups[3].Value.ElementAt(0), int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+			});
+		}
+
+		private static void Day2A(IEnumerable<Password> passwords)
+		{
+			int valids = 0;
+
+			foreach (Password pw in passwords)
+			{
+				int count = pw.Text.Count(c => c == pw.Checker);
+				if (count >= pw.Min && count <= pw.Max)
+					valids++;
+			}
 
 			Console.WriteLine($"\nThe solution to Day2A is {valids}.");
 		}
 
-		private static void Day2B(IEnumerable<string> lines)
+		private static void Day2B(IEnumerable<Password> passwords)
 		{
-			Regex pwRegex = new Regex(@"(\d+)-(\d+) (\w): (\w+)");
 			int valids = 0;
 
-			foreach (string line in lines)
-			{
-				Match match = pwRegex.Match(line);
-
-				char checker = match.Groups[3].Value.ElementAt(0);
-				int pos1 = int.Parse(match.Groups[1].Value) - 1;
-				int pos2 = int.Parse(match.Groups[2].Value) - 1;
-
-				if(match.Groups[4].Value.ElementAt(pos1) == checker ^ match.Groups[4].Value.ElementAt(pos2) == checker)
+			foreach (Password pw in passwords)
+			{ 
+				if (pw.Text.ElementAt(pw.Min - 1) == pw.Checker ^ pw.Text.ElementAt(pw.Max - 1) == pw.Checker)
 				{
 					valids++;
 				}
